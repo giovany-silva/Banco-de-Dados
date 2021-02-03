3797da643ba79898b408480d1599ca7e8274a78a
giovanysilva93 <69771585+giovanysilva93@users.noreply.github.com>
2021-02-03 13:07:40 -0300

    2

---
+++ Sistema Login/validar.php
@@ -0,0 +1,19 @@
+<?php
+
+$login=$_POST["usuario"];
+
+$senha=$_POST["senha"];
+
+if (($login == "USUARIO") && ($id2 == "SENHA")){
+
+<meta http-equiv="refresh" content="5; URL='localhost/usuario.php'"/>
+
+}else {
+
+echo '<script type="text/javascript>alert("Usuario ou Senha invalido!")</script>';
+
+}
+
+?>
+
+
